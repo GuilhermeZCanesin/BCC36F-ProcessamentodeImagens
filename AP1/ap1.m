30a323ff04e1f9b6de09a5c634ede1402013bfd4
Andre Monegat <andre.monegat@gmail.com>
2022-03-28 10:54:18 -0300

    funcionando

--- AP1/ap1.m
+++ AP1/ap1.m
@@ -1,12 +1,8 @@
 pkg load image;
 shaded_image = imread("tungsten_filament_shaded.tif");
-image(shaded_image)
 shadding_mask = imread("tungsten_sensor_shading.tif");
-image(shadding_mask)
-
-x = im2double (shaded_image)
-y = im2double (shadding_mask)
-
-z = x ./ y
-
-image (z)
+x = im2double (shaded_image);
+y = im2double (shadding_mask);
+z = x ./ y;
+imshow (z)
+imwrite(z,"shadded.tif")
