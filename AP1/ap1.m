bf4f9c0fa3ac74c53a5e02824a3c61462d5d5ef7
Andre Monegat <andre.monegat@gmail.com>
2022-03-28 09:55:45 -0300

    octave não ta ajudando mas creio que era isso

--- AP1/ap1.m
+++ AP1/ap1.m
@@ -1,4 +1,12 @@
+pkg load image;
 shaded_image = imread("tungsten_filament_shaded.tif");
-image(shaded_image);
+image(shaded_image)
 shadding_mask = imread("tungsten_sensor_shading.tif");
-image(shadding_mask);
+image(shadding_mask)
+
+x = im2double (shaded_image)
+y = im2double (shadding_mask)
+
+z = x ./ y
+
+image (z)
